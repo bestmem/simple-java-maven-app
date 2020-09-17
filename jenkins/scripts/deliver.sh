5c4e06f342bbabeb69d5f56022a6d8c9e9a97019
bestmem <774138110@qq.com>
2020-09-17 20:03:26 +0800

    change deliver

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -4,6 +4,8 @@ echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
+echo 'hostname: use docker or host'
+echo hostname
 set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
